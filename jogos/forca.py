8d629655cb546c13d4920676b4b012f866719085
Igor Dallazen Follador <igor_follador@sicredi.com.br>
2022-04-22 15:41:05 -0300

    Treating inputs and iterating words

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,6 +3,19 @@ def iniciar():
     print("Bem vindo ao jogo da Forca!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+        chute = input("Qual a letra?\n")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                print("Encontrei a letra {} na posição {}".format(letra.upper(), index))
+            index += 1
     print("Fim do jogo")
 
 
