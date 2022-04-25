f0704fa571b081bbfbe4e021e3a30daaa61df47b
Igor Dallazen Follador <igor_follador@sicredi.com.br>
2022-04-25 15:20:36 -0300

    Organizing and subdividing code

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,39 +1,22 @@
 import random
 
+
 def iniciar():
-    print("*********************************")
-    print("Bem vindo ao jogo da Forca!")
-    print("*********************************")
+    imprime_mensagem_abertura()
+    palavra_secreta = sorteia_palavra("palavras.txt")
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+    print(letras_acertadas)
 
-    arquivo = open("palavras.txt", "r")
-    palavras = []
-
-    for linha in arquivo:
-        linha = linha.strip().upper()
-        palavras.append(linha)
-    arquivo.close()
-
-    index = random.randrange(0, len(palavras)) #Sorteia o número referente ao local da palavra no array
-    palavra_secreta = palavras[index].upper() #Seleciona a palavra sorteada e realiza a conversão da mesma para caixa alta
-    letras_acertadas = ["_" for letra in palavra_secreta] #Add um caracter a cada letra da palavra
-
+    tentativas = 6  # Define o valor de tentativas que o usuário pode errar
     enforcou = False
     acertou = False
     erros = 0
-    tentativas = 6
-
-    print(letras_acertadas)
 
     while not enforcou and not acertou:
-        chute = input("Qual a letra?\n")
-        chute = chute.strip().upper()
+        chute = le_chute()
 
         if chute in palavra_secreta:
-            index = 0
-            for letra in palavra_secreta:
-                if chute == letra:
-                    letras_acertadas[index] = letra
-                index += 1
+            salva_chute_correto(chute, letras_acertadas, palavra_secreta)
         else:
             erros += 1
             print("Ops, você errou! Faltam {} tentativas.".format(tentativas - erros))
@@ -42,10 +25,52 @@ def iniciar():
         print(letras_acertadas)
 
     if acertou:
-        print("Você ganhou!")
+        imprime_mensagem_vencedor()
     else:
-        print("Você perdeu!")
+        imprime_mensagem_perdedor()
     print("Fim do jogo")
+
+
+def imprime_mensagem_vencedor():
+    print("Você GANHOU!")
+
+
+def imprime_mensagem_perdedor():
+    print("Você PERDEU!")
+
+def salva_chute_correto(chute, letras_acertadas, palavra):
+    index = 0
+    for letra in palavra:
+        if chute == letra:
+            letras_acertadas[index] = letra
+        index += 1
+
+
+def le_chute():
+    chute = input("Qual a letra?\n")
+    chute = chute.strip().upper()
+    return chute
+
+
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]  # Add um caracter a cada letra da palavra
+
+
+def sorteia_palavra(documento: str):
+    palavras = []
+
+    with open(documento) as arquivo:
+        for linha in arquivo:
+            palavras.append(linha.strip().upper())
+
+    index = random.randrange(0, len(palavras))  # Sorteia o número referente ao local da palavra no array
+    return palavras[index].upper()  # Retorna a palavra sorteada e realiza a conversão da mesma para caixa alta
+
+
+def imprime_mensagem_abertura():
+    print("*********************************")
+    print("Bem vindo ao jogo da Forca!")
+    print("*********************************")
 
 
 if __name__ == "__main__":
