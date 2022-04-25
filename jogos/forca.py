1e3f147db9647fac36e62305f270293c4902ca10
Igor Dallazen Follador <igor_follador@sicredi.com.br>
2022-04-25 15:28:27 -0300

    Improve user presentation

--- jogos/forca.py
+++ jogos/forca.py
@@ -7,7 +7,7 @@ def iniciar():
     letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
     print(letras_acertadas)
 
-    tentativas = 6  # Define o valor de tentativas que o usuário pode errar
+    tentativas = 7  # Define o valor de tentativas que o usuário pode errar
     enforcou = False
     acertou = False
     erros = 0
@@ -19,7 +19,7 @@ def iniciar():
             salva_chute_correto(chute, letras_acertadas, palavra_secreta)
         else:
             erros += 1
-            print("Ops, você errou! Faltam {} tentativas.".format(tentativas - erros))
+            desenha_forca(erros, tentativas)
         enforcou = erros == tentativas
         acertou = "_" not in letras_acertadas
         print(letras_acertadas)
@@ -27,16 +27,96 @@ def iniciar():
     if acertou:
         imprime_mensagem_vencedor()
     else:
-        imprime_mensagem_perdedor()
+        imprime_mensagem_perdedor(palavra_secreta)
     print("Fim do jogo")
 
 
 def imprime_mensagem_vencedor():
-    print("Você GANHOU!")
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
 
 
-def imprime_mensagem_perdedor():
-    print("Você PERDEU!")
+def imprime_mensagem_perdedor(palavra_secreta):
+    print("Puxa, você foi enforcado!")
+    print("A palavra era {}".format(palavra_secreta))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+def desenha_forca(erros, tentativas):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+    print("Ops, você errou! Faltam {} tentativas.".format(tentativas - erros))
+    print()
+
 
 def salva_chute_correto(chute, letras_acertadas, palavra):
     index = 0
